497de8d88d773bcd85c683153469b4dad253d01e
Samadrita05 <rajeshhazra879@gmail.com>
2026-01-19 13:10:11 +0530

    Create role_skills.py

---
+++ app/data/role_skills.py
@@ -0,0 +1,30 @@
+ROLE_SKILLS = {
+    "Data Analyst": [
+       "excel", "power bi", "python", "statistics", "pandas", "data visualization", "tableau"],
+    "Data Scientist": [
+        "python", "machine learning", "statistics", "numpy", "pandas", "deep learning", "data cleaning"],
+    "Backend Developer": [
+        "python", "java", "node", "sql", "api", "django", "fastapi", "spring"],
+    "Frontend Developer": [
+        "html", "css", "javascript", "react", "typescript", "UI", "UX"],
+    "Software Developer": [
+        "c++", "java", "data structures", "algorithms", "git", "system design", "linux", "oop"],
+    "DevOps Developer": [
+        "linux", "docker", "kubernetes", "aws", "jenkins", "terraform", "bash", "cicd"],
+    "Cloud Developer": [
+        "aws", "azure", "google cloud", "iam", "networking", "serverless", "microservices", "monitoring"],
+    "Mobile Developer": [
+        "swift", "kotlin", "flutter", "firebase", "ios", "android", "app deployment", "mobile ui","api integration"],
+    "Game Developer": [
+        "c++", "unity", "unreal engine", "3d math", "physics", "shaders"],
+    "AI/ML Engineer": [
+        "python", "tensorflow","pytorch","machine learning","deep learning", "data preprocessing", "model deployment","numpy", "pandas","artificial intelligence"],
+    "Database Engineer": [
+        "dbms", "mysql", "postgresql", "normalization", "indexing", "query optimization", "transactions", "acid"],
+    "Cyber Security Analyst": [
+        "network security", "linux", "penetration testing", "vulnerabilities", "phishing", "firewalls", "encryption", "siem"],
+    "QA Engineer": [
+        "manual testing", "automation testing", "selenium", "test cases", "bug tracking","postman"],
+    "AI Researcher": [
+        "artificial intelligence", "deep learning","neural networks", "research papers", "discrete math", "optimization", "pytorch"]
+}
